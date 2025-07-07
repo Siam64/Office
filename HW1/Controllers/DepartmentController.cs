05304a6bcbd6b3bc39b4c4ca41e614e38d5870e9
Samiul Alim <siamahmed64038@gmail.com>
2025-07-07 09:41:21 +0600

    initial commit

---
+++ HW1/Controllers/DepartmentController.cs
@@ -0,0 +1,121 @@
+﻿using HW.Application.Interface;
+using HW.Application.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW1.Project.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+        public IActionResult DepIndex()
+        {
+            var departments = _departmentRepository.GetAll() ?? new List<DepartmentDto>();
+            return View(departments);
+        }
+
+        public IActionResult AddDepartment(DepartmentDto model)
+        {
+            if (model == null)
+            {
+                return Json(new { success = false, message = "No data to save" });
+            }
+
+            try
+            {
+                var result = _departmentRepository.Add(model);
+                if (result == 0)
+                {
+                    return Json(new { success = false, message = "Saving Error" });
+                }
+
+                var dataResult = _departmentRepository.GetById(result);
+                return Json(new { success = true, message = "Vauchar created successfully", data = dataResult });
+
+            }
+            catch
+            {
+                return Json(new { success = false, message = "Error" });
+            }
+        }
+
+
+        public IActionResult GetDataForUpdate(int id)
+        {
+            if (id <= 0)
+            {
+                return Json(new { success = false, message = "Invalid ID" });
+            }
+
+            try
+            {
+                var data = _departmentRepository.GetById(id);
+                if (data == null)
+                {
+                    return Json(new { success = false, message = "Department not found" });
+                }
+                return Json(new { success = true, data = data });
+            }
+            catch
+            {
+                return Json(new { success = false, message = "Error retrieving department" });
+
+            }   
+        
+        }
+
+        public IActionResult Update(DepartmentDto model)
+        {
+            if (model == null)
+            {
+                return Json(new { success = false, message = "Error retrieving department" });
+            }
+
+            try
+            {
+                var data = _departmentRepository.Update(model);
+                if (data == null)
+                {
+                    return Json(new { success = false, message = "Error updating department" });
+                }
+
+                return Json(new { success = true, data = data });
+            }
+            catch
+            {
+                return Json(new { success = false, message = "Exception occurred while updating department" });
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0)
+            {
+                return Json(new { success = false, message = "Invalid ID" });
+            }
+
+            try
+            {
+                var result = _departmentRepository.Delete(id);
+
+                if (!result)
+                {
+                    return Json(new { success = false, message = "Error deleting department" });
+                }
+
+                return Json(new { success = true, message = "Department deleted successfully" });
+            }
+            catch
+            {
+                return Json(new { success = false, message = "Error deleting department" });
+            }
+        }
+
+    }
+}
---
+++ HW1/Controllers/EmployeesController.cs
@@ -0,0 +1,124 @@
+﻿using HW.Application.Interface;
+using HW.Application.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW1.Project.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public EmployeesController(IDepartmentRepository departmentRepository, IEmployeeRepository employeeRepository)
+        {
+            _departmentRepository = departmentRepository;
+            _employeeRepository = employeeRepository;
+        }
+        public IActionResult EmpIndex()
+        {
+            var result = _departmentRepository.GetAll();
+            ViewBag.Departments = result.Select(d => d.DepName).ToList();
+
+            var Employees = _employeeRepository.GetAll() ?? new List<EmployeeDto>();
+            return View(Employees);
+        }
+
+
+        public IActionResult AddEmployee(EmployeeDto model) {
+
+            if (model == null)
+            {
+                return Json(new { success = false, message = "No data to save" });
+            }
+
+            try 
+            {
+                var result = _employeeRepository.Add(model);
+                if (result == 0)
+                {
+                    return Json(new { success = false, message = "Saving Error" });
+                }
+                var dataResult = _employeeRepository.GetById(result);
+                return Json(new { success = true, message = "Employee created successfully", data = dataResult });
+            }
+            catch
+            {
+                return Json(new { success = false, message = "Error" });
+
+            }
+        
+        }
+
+
+        public IActionResult GetDataForUpdate(int id)
+        {
+
+            if (id == 0)
+            {
+                return Json(new { success = false, message = "Error" });
+            }
+
+            try
+            {
+                var data = _employeeRepository.GetById(id);
+                if (data == null)
+                {
+                    return Json(new { success = false, message = "Employee not found" });
+                }
+                return Json(new { success = true, data = data });
+            }
+            catch
+            {
+               return Json(new { success = false, message = "Error" });
+            }
+            
+        }
+
+        public IActionResult Update(EmployeeDto model)
+        {
+            if(model == null)
+            {
+                return Json(new {success = false, message = "Error"});
+            }
+
+            try
+            {
+                var data = _employeeRepository.Update(model);
+                if (data == null)
+                {
+                    return Json(new { success = false, message = "Failed" });
+                }
+                return Json(new { success = true, data = data });
+            }
+            catch
+            {
+                return Json(new { success = false, message = "Error" });
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if(id == 0)
+            {
+                return Json(new { success = false, message = "Error" });
+            }
+
+            try
+            {
+                var result = _employeeRepository.Delete(id);
+
+                if (!result)
+                {
+                    return Json(new { success = false, message = "Error deleting employee" });
+                }
+
+                return Json(new { success = true, message = "Employee deleted successfully" });
+            }
+            catch
+            {
+                return Json(new { success = false, message = "Error deleting employee" });
+            }
+        }
+
+    }
+}
